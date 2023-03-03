ca7093b314c911b3e8ecc5de30b7f5b1fbf7a7dd
Jacob Hummer <jcbhmr@outlook.com>
2023-03-03 23:35:03 +0000

    Try to install a newer version of cmake

---
+++ .devcontainer/features/cmake/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+# https://askubuntu.com/a/865294
+
+apt remove --purge --auto-remove cmake
+
+apt update
+apt install -y software-properties-common lsb-release
+apt clean all
+
+wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
+
+apt-add-repository "deb https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main"
+
+apt update
+apt install kitware-archive-keyring
+rm /etc/apt/trusted.gpg.d/kitware.gpg
+
+apt update
+apt install cmake
