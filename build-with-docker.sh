ff8cfe0989aaa68d466b933883402101e606b377
Jacob Hummer <jcbhmr@outlook.com>
2023-03-04 03:06:33 +0000

    Try building in Docker container

---
+++ build-with-docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+docker build -t emception_build docker
+mkdir -p build/emsdk_cache
+docker run -it --rm \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v $(pwd):$(pwd) \
+    -v $(pwd)/build/emsdk_cache:/emsdk/upstream/emscripten/cache \
+    -u $(id -u):$(id -g) \
+    $(id -G | tr ' ' '\n' | xargs -I{} echo --group-add {}) \
+    emception_build:latest \
+    bash -c "cd $(pwd) && ./build.sh"
