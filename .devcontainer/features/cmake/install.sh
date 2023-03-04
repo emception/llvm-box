b03d8722317d7e729fcdbebaaf5e5c369e4f01fb
Jacob Hummer <jcbhmr@outlook.com>
2023-03-04 02:01:10 +0000

    Fix devcontainer speed #2

--- .devcontainer/features/cmake/install.sh
+++ .devcontainer/features/cmake/install.sh
@@ -4,12 +4,6 @@ set -e
 # Simulate feature options
 export VERSION=3.26.0-rc5
 
-# https://askubuntu.com/a/865294
-mkdir "/opt/cmake-$VERSION"
-cd "/opt/cmake-$VERSION"
-wget "https://cmake.org/files/v$VERSION/cmake-$VERSION.tar.gz"
-tar -xzvf "cmake-$VERSION.tar.gz"
-cd "cmake-$VERSION"
-./bootstrap
-make "-j$(nproc)"
-make install
+curl -fsSLo install-cmake.sh "https://github.com/Kitware/CMake/releases/download/v$VERSION/cmake-$VERSION-linux-$(uname -m).sh"
+chmod +x install-cmake.sh
+./install-cmake.sh --skip-license --prefix=/usr/local --exclude-subdir
